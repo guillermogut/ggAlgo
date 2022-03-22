235b4ec91a3d036329bd9fdd57f79d1827af76ab
Gui Gutierrez <guillermogut@umass.edu>
2022-03-22 15:17:35 -0400

    made base grid

--- src/App.js
+++ src/App.js
@@ -4,32 +4,203 @@ import Grid from './components/Grid'
 import { useEffect, useReducer,useState } from 'react';
 import { createStore } from 'redux';
 import { reducer } from './reducer'
+import Node from './components/Node'
 
-let rows = 10;
-let cols = 10;
-let defaultState = {
-  grid: new Array(rows).fill(new Array(cols).fill(0)),
-  select: false,
-  setWall: false
+const makeNode = (row,column) =>{
+    return {row,column,isWall: false,isStart:false,isEnd:false,cost:1}
+
 }
-function App() {
 
-  const [state,dispatch] = useReducer(reducer,defaultState)
+const initalizeGrid = () => {
+  let grid = [];
+  for (let i = 0; i < 20; i++){
+    let row=[]
+    
+    for (let j = 0; j < 50; j++){
+      row.push(makeNode(i, j));
+    }
+    grid.push(row);
+  }
+
+  return grid;
+}
+
+
+
+function App() {
+  //console.log('reeeee')
+  //const [state,dispatch] = useReducer(reducer,defaultState)
+  const [grid, setGrid] = useState(initalizeGrid());
+  const [mouseDown, setMouseDown] = useState(false);
+  const [start, setStart] = useState(false);
+  const [end, setEnd] = useState(false);
+
   
+  const changeGrid = (grid,row,col) => {
+  
+    //make sure to reset node isWall and isEnd as well
+    if (start)
+    {
+      let node = grid[row][col];
+  
+      const newNode = { ...node };
+      newNode.isWall = false;
+      newNode.isStart = true;
+      newNode.isEnd = false;
+      let newGrid = [...grid]
+      newGrid[row][col] = {...newNode};
+      setStart(false);
+      return newGrid;
+    }
+    if (end)
+    {
+      let node = grid[row][col];
+  
+      const newNode = { ...node };
+      newNode.isWall = false;
+      newNode.isStart = false;
+      newNode.isEnd = true;
+      let newGrid = [...grid]
+      newGrid[row][col] = {...newNode};
+      setEnd(false)
+      return newGrid;
+    }
+  if (row === -1)
+    {
+      return null
+    }
+  //let newGrid = grid.slice();
+  
+    let node = grid[row][col];
+  
+    const newNode = { ...node };
+    newNode.isWall = !newNode.isWall;
+    newNode.isEnd = false;
+    newNode.isStart = false;
+    let newGrid = [...grid]
+    newGrid[row][col] = {...newNode};
+  
+  return newGrid;
+  }
+  
+  const handlePlaceStart = () => {
+    setStart(!start);
+  }
+  const handlePlaceEnd = () => {
+    setEnd(!end);
+  }
+  const handleMouseEnter = (row,col) => {
+  //change color if mouse is down for node
+    //console.log('mouse status: '+ mouseDown)
+    
+    if (!mouseDown) {
+      //do nothing, just return
+      //console.log('in mouseEnter mouse up')
+      return
+    }
+    else {
+      //console.log("in mouseEnter mouse down")
+    //otherwise change the grid from top to bottom
+    //const newGrid = changeGrid(grid, row, col);
+    //setGrid(...changeGrid(grid, row, col));
+      const newGrid = changeGrid(grid, row, col)
+      if(newGrid === null) return
+      setGrid([...newGrid]);
+    }
+    
+  }
+
+  const handleMouseLeave = (row, col) => {
+  
+  }
+
+  const handleMouseUp = () => {
+    //console.log('in handle mouse up')
+    const changed = false;
+    setMouseDown(changed);
+    
+  }
+
+  const handleMouseDown = (row,col) => {
+    
+    
+    //const newGrid = changeGrid(grid, row, col);
+    if (row === -1) {//for testing handlMouseDown in another component
+      //console.log('in handle mouse down '+ row)
+      //console.log(row)
+      const changed = true;
+      setMouseDown(changed)
+      
+    }
+    else {
+      const changed = true;
+    
+      setMouseDown(changed)
+
+    }
+    
+    const newGrid = changeGrid(grid, row, col)
+    if(newGrid === null) return
+    setGrid([...newGrid]);
+  }
+
+
   useEffect(() => {
-    console.log(state.grid)
-    setTimeout(function () {
-      dispatch({type:'ree', payload:{i:5,j:5}})
-    }, 2000);
-    console.log(state.grid)
+    //console.log(grid.length)
+    //setGrid(initalizeGrid());
+    
+    //console.log("mousedown: "+mouseDown)
   },[])
   //const gridArr = new Array(10).fill(new Array(10).fill(0));
   
   
   return (
-    <div className="App">
-      <header className="App-header">
-        <Grid grid={state.grid}></Grid>
+    <div className="App" >
+      
+      <header className="App-header" onMouseUp={handleMouseUp} onMouseDown={() => handleMouseDown(-1,-1)} >
+        
+        <div className="rowContainer">
+          <button type='button' onClick={() =>handlePlaceStart()}>Place Start Node</button>
+          <button type = 'button' onClick={() => handlePlaceEnd()}>Place End Node</button>
+        </div>
+        {
+
+          
+          grid.map((rows, rowIndex) => {
+            
+            return (
+              <div className='gridRow' key = {rowIndex}>
+
+                {
+                  rows.map((node, colIndex) => {
+                    return (
+                      <div key={colIndex}>
+                        
+                        {
+                          <Node 
+                            key ={colIndex}
+                            onMouseEnter={(rowIndex,colIndex) => handleMouseEnter(rowIndex,colIndex)}
+                            onMouseLeave ={() => handleMouseLeave}
+                            onMouseDown={(rowIndex,colIndex) => handleMouseDown(rowIndex,colIndex)}
+                            onMouseUp={() =>handleMouseUp()}
+                            node={{...node}}></Node>
+                        }
+                      </div>
+                    )
+                        })
+
+
+                  
+                }
+
+              </div>
+
+            )
+            
+          })
+          
+        }
+        { console.log(grid)}
       </header>
     </div>
   );
--- src/components/Node.js
+++ src/components/Node.js
@@ -5,36 +5,62 @@ import { reducer } from '../reducer'
 const Node = (props) => {
 
     
-    //console.log(props)
+    if (props.node.row === 0 && props.node.column === 0)
+    {
+        //console.log(props.node.isWall.toString())
+        //console.log(props.node.isWall + "node "+props.node.row + " "+ props.node.column)    
+    }
+    const [node, setNode] = useState(props.node);
+    
     const [color, setColor] = useState('#949599')//start white
-    const [i, setI] = useState(props.rowNum);
-    const [j, setJ] = useState(props.colNum);
+    const [i, setI] = useState(props.node.row);
+    const [j, setJ] = useState(props.node.column);
     const [state, dispatch] = useReducer(reducer);
+    const [isWall, setIsWall] = useState(props.node.isWall);
+    const [isStart, setIsStart] = useState(props.isStart);
+    const [isEnd, setIsEnd] = useState(props.isEnd);
+    const [cost, setCost] = useState(props.cost)
+    
+    //const [mouseDown, setMouseDown] = useState(false);
     //console.log(state.grid)
     //blueish #4f5157
-
-    const handleMouseEnter = () => {
+    
+    useEffect(() => {
         
-        if (color == '#949599')
+        //console.log('in the effect')
+        //setNode(node)
+        if (props.node.isWall)
         {
-            setColor('#4f5157')
+           // console.log("is a wall")
+            const newColor = '#4f5157';
+            setColor(newColor);
+        }
+        else if (props.node.isStart)
+        {
+            const newColor = '#bba907';
+            setColor(newColor);
+        }
+        else if (props.node.isEnd)
+        {
+            const newColor = '#5a1104';
+            setColor(newColor);
         }
         else {
-            setColor('#949599')
+            const newColor = '#949599';
+            setColor(newColor)
         }
-    }
-
-    const handleMouseLeave = () => {
-        
-    }
+    }, [props.node]);
     return (<>
-        <div onMouseLeave={handleMouseLeave}
-            onMouseEnter={handleMouseEnter}
+        
+        <div 
+            
+            onMouseEnter={() => props.onMouseEnter(i, j)}
+            onMouseDown={() => props.onMouseDown(i, j)}
+            onMouseUp = {() => props.onMouseUp()}
             className='node'
             style={ {backgroundColor: color}}>
-            {
-               
-            }
+
+            
         </div>
     
     </>)
